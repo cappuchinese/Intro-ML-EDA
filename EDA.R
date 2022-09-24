103f8434171f349dbe15c7c819f71b57cd54bc3a
Lisa Hu <ljbhu2002@gmail.com>
2022-09-24 19:24:29 +0200

    Figuring out dependant variable with no success

--- EDA.R
+++ EDA.R
@@ -11,39 +11,96 @@
 ## ---------------------------
 
 ## ---- read-data ----
-dataset <- read.csv("Data.csv")
-codebook <- read_delim("codebook.txt", delim = "|")
-knitr::kable(codebook[1:4], booktabs = T, caption = "Data values")
-knitr::kable(codebook[c(1,5)], booktabs = T, caption = "Description")
+dataset <- read.csv("data/Data.csv")
+codebook <- read_delim("data/codebook.txt", delim = "|")
+pander(codebook[1:4], booktabs = T, caption = "Data values", split.tables = 100)
+pander(codebook[c(1,5)], booktabs = T, caption = "Description",
+       justify = c("right", "left"), split.tables = 100)
 
 ## ---- change-data ----
 # Change the empty strings to NA
 dataset[dataset == ""] <- NA
 
 # Remove unnecessary columns
-drop <- c("sample_id", "benign_sample_diagnosis")
+drop <- c("sample_id", "patient_cohort", "sample_origin", "benign_sample_diagnosis")
 dataset <- dataset[,!(names(dataset) %in% drop)]
 
-# Different diagnosis groups
+pander(summary(dataset), split.table = 100)
+
+## ---- log-trans ----
+log.data <- log(dataset[5:10] +1)
+colnames(log.data) <- paste("log", names(dataset[5:10]), sep = ".")
+dataset <- cbind(dataset, log.data)
+
+## ---- demograph ----
+# Different diagnosis and blood groups
 control <- subset(dataset, diagnosis == 1)
 benign <- subset(dataset, diagnosis == 2)
 pdac <- subset(dataset, diagnosis == 3)
+blood <- subset(dataset, plasma_CA19_9 >= 0)
+
+# Delete all the entries with blood sample and drop "plasma" columns
+dataset <- dataset[-which(dataset$plasma_CA19_9 >= 0),]
+dataset <- dataset[,-c(5, 11)]
 
 # Demographics
 demograph <- data.frame(c(sum(control$sex == "F"), sum(control$sex == "M")),
                         c(sum(benign$sex == "F"), sum(benign$sex == "M")),
                         c(sum(pdac$sex == "F"), sum(pdac$sex == "M")))
+
+blood.demo <- data.frame(c(sum(blood$sex == "F" & blood$diagnosis == 1),
+                           sum(blood$sex == "M" & blood$diagnosis == 1)),
+                         c(sum(blood$sex == "F" & blood$diagnosis == 2),
+                           sum(blood$sex == "M" & blood$diagnosis == 2)),
+                         c(sum(blood$sex == "F" & blood$diagnosis == 3),
+                           sum(blood$sex == "M" & blood$diagnosis == 3)))
+
+colnames(blood.demo) <- c("Control", "Benign", "PDAC")
 colnames(demograph) <- c("Control", "Benign", "PDAC")
-rownames(demograph) <- c("Female", "Male")
+demograph <- rbind(demograph, blood.demo)
+rownames(demograph) <- c("Female total", "Male total", "Female blood", "Male blood")
 
-knitr::kable(demograph)
+pander(demograph, booktabs = T, caption = "Demographic of the samples",
+       justify = c("left", "center", "center", "center"))
 
-# Amount of blood plasma samples
-nrow(dataset) - sum(length(which(is.na(dataset$plasma_CA19_9))))
-blood <- subset(dataset, plasma_CA19_9 >= 0)
+## ---- explore-var ----
+# Summary dataset
+pander(summary(dataset), split.tables = 100)
 
-## ---- EDA
+## ---- exp-box ----
+# Boxplot function
+create.plots <- function(y.values, y.label, plt.tag) {
+  list(ggplot(data = control, aes(x = diagnosis, y = !!sym(y.values))) +
+    geom_boxplot(outlier.color = "red", outlier.shape = 1, aes(fill = "Control")) +
+    geom_boxplot(data = benign, outlier.color = "red", outlier.shape = 1,
+                 aes(fill = "Benign")) +
+    geom_boxplot(data = pdac, outlier.color = "red", outlier.shape = 1,
+                 aes(fill = "PDAC")) +
+    labs(x = "Diagnosis", y = y.label, tag = plt.tag) +
+    scale_fill_manual(values = c("red", "green", "blue"),
+                      limits = c("Control", "Benign", "PDAC"),
+                      name = ""))
+}
 
+# Create the boxplots for the different columns
+y.values <- names(dataset[10:14])
+y.labs <- c("log(Creatinine) (mg/ml)", "log(LYVE1) (ng/ml)", "log(REG1B) (ng/ml)",
+            "log(TFF1) (ng/ml)", "log(REG1A) (ng/ml)")
+plt.tag <- c("a", "b", "c", "d", "e")
+plts <- mapply(create.plots, y.values, y.labs, plt.tag)
+
+# Grid and print the plots
+p1 <- ggarrange(plotlist = plts[1:2], ncol = 2,
+                common.legend = TRUE, legend = "top")
+p2 <- ggarrange(plotlist = plts[3:5], ncol = 3,
+                common.legend = TRUE, legend = "top")
+my.grid <- ggarrange(p1, p2, nrow = 2)
+print(annotate_figure(my.grid))
+
+## ---- EDA ----
+ggplot(pdac, aes(x = age, y = stage, color = stage)) + geom_point()
+pca <- prcomp(dataset[,6:9], center = TRUE, scale. = TRUE)
+ggbiplot(pca, obs.scale = 1, var.scale = 1, groups = dataset$diagnosis, ellipse = F, circle = T)
 ## Assign age categories
 # dataset <- dataset %>%
 #   mutate(
