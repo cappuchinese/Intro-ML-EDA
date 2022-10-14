6f102345c468a729ef90a1a441ae85c872527ee0
Lisa Hu <ljbhu2002@gmail.com>
2022-10-14 15:07:10 +0200

    Feedback tip3: Table with values for readability

--- report/EDA_vars.R
+++ report/EDA_vars.R
@@ -31,8 +31,8 @@ dataset <- dataset %>%
     ## Factor for order of age
     diagnosis_group = factor(
       dplyr::case_when(
-        diagnosis == 1 ~ "Control/Benign",
-        diagnosis == 2 ~ "Control/Benign",
+        diagnosis == 1 ~ "Control",
+        diagnosis == 2 ~ "Benign",
         stage == "I" ~ "I-IIA",
         stage == "IA" ~ "I-IIA",
         stage == "IB" ~ "I-IIA",
@@ -41,12 +41,23 @@ dataset <- dataset %>%
         stage == "IIB" ~ "I-II",
         stage == "III" ~ "III-IV",
         stage == "IV" ~ "III-IV" ),
-      level = c("Control/Benign", "I-IIA", "I-II", "III-IV")
+      level = c("Control", "Benign", "I-IIA", "I-II", "III-IV")
     )
   )
 
+# Perform the tests
 REG1A <- dunnTest(dataset$REG1A ~ dataset$diagnosis_group)
 REG1B <- dunnTest(dataset$REG1B ~ dataset$diagnosis_group)
+
+# Create a nice format to show the correct comparisons
+comparison <- t(cbind(REG1A$res[c(2:5,7,8),c(1,4)], REG1B$res[c(2:5,7,8),4]))
+colnames(comparison) <- comparison[1,]
+comparison <- comparison[-1,]
+comparison <- apply(comparison, 2, as.numeric)
+rownames(comparison) <- c("REG1A", "REG1B")
+comparison[comparison > 0.05] <- "ns"
+
+# Drop the REG1A
 dataset <- dataset[,!(names(dataset) %in% "REG1A")]
 
 # Log transform
