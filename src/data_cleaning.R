538e82ac434976c01615570342cc54dfb3618910
cappuchinese <ljbhu2002@gmail.com>
2022-11-02 15:22:38 +0100

    Better structure and data imbalance

--- src/data_cleaning.R
+++ src/data_cleaning.R
@@ -22,9 +22,24 @@ dataset <- read.csv("../data/Data.csv")
 dataset[dataset == ""] <- NA
 
 # Group the samples
-dataset$diagnosis <- factor(dataset$diagnosis, levels = unique(dataset$diagnosis),
-                            labels = c("Control", "Benign", "PDAC"))
 dataset$sex <- factor(dataset$sex)
+dataset <- dataset %>%
+  mutate(
+    diagnosis = factor(
+      dplyr::case_when(
+        diagnosis == 1 ~ "Control",
+        diagnosis == 2 ~ "Benign",
+        stage == "I" ~ "PDAC early",
+        stage == "IA" ~ "PDAC early",
+        stage == "IB" ~ "PDAC early",
+        stage == "II" ~ "PDAC early",
+        stage == "IIA" ~ "PDAC early",
+        stage == "IIB" ~ "PDAC early",
+        stage == "III" ~ "PDAC late",
+        stage == "IV" ~ "PDAC late"),
+      level = c("Control", "Benign", "PDAC early", "PDAC late")
+    )
+  )
 
 # Drop unnecessary columns
 drop <- c("sample_id", "patient_cohort", "sample_origin", "benign_sample_diagnosis",
@@ -46,19 +61,20 @@ train.rows <- sort(sample(seq_len(nrow(dataset)), size = floor(0.7*nrow(dataset)
 training <- dataset[train.rows,]
 test <- dataset[-train.rows,]
 
-control.train <- subset(training,
-                        training$diagnosis == "Control" | training$diagnosis == "PDAC")
-benign.train <- subset(training,
-                       training$diagnosis == "Benign" | training$diagnosis == "PDAC")
-control.test <- subset(test, test$diagnosis == "Control" | training$diagnosis == "PDAC")
-benign.test <- subset(test, test$diagnosis == "Benign" | training$diagnosis == "PDAC")
+control <- subset(training,
+                        training$diagnosis == "Control" |
+                          training$diagnosis == "PDAC early" |
+                          training$diagnosis == "PDAC late")
+benign <- subset(training,
+                       training$diagnosis == "Benign" |
+                         training$diagnosis == "PDAC early" |
+                         training$diagnosis == "PDAC late")
 
 # Export dataset
-write.csv(dataset, "../data/cleaned_data.csv", row.names = F, quote = F, na="")
-write.csv(control.train, "../data/control_train.csv", row.names = F, quote = F, na="")
-write.csv(control.test, "../data/control_test.csv", row.names = F, quote = F, na="")
-write.csv(benign.train, "../data/benign_train.csv", row.names = F, quote = F, na="")
-write.csv(benign.test, "../data/benign_test.csv", row.names = F, quote = F, na="")
+write.csv(dataset, "../data/wekafiles/cleaned_data.csv", row.names = F, quote = F, na="")
+write.csv(control, "../data/wekafiles/control_train.csv", row.names = F, quote = F, na="")
+write.csv(benign, "../data/wekafiles/benign_train.csv", row.names = F, quote = F, na="")
+write.csv(test, "../data/wekafiles/test.csv", row.names = F, quote = F, na="")
 
 # Set working directory back to log folder
 setwd("../log")
